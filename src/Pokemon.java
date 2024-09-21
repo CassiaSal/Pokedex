9c3bbb540dce3a53647ed3a2671ad9c46b1d899d
CassiaSal <cassia.salmon@gmail.com>
2024-09-21 13:27:19 +1200

    Create Pokemon object

---
+++ src/Pokemon.java
@@ -0,0 +1,20 @@
+package src;
+/**
+ * Represents a Pokémon with its attributes.
+ * This record stores the ID, name, type, height, and weight of a Pokémon.
+ */
+public record Pokemon(int ID, String name, String type, double height, double weight) {
+    /**
+     * Returns a string representation of the Pokémon.
+     *
+     * @return a formatted string containing the Pokémon's details.
+     */
+    @Override
+    public String toString() {
+        return "ID: " + ID +
+               " | Name: " + name +
+               " | Type: " + type +
+               " | Height: " + height +
+               " | Weight: " + weight;
+    }
+}
